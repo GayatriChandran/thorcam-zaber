2c379cffa8b9c31f124cf7cb585fa256a60287af
Chandran <gpc4@illinois.edu>
2025-07-19 15:57:12 -0500

    Display cropped image in custom range

--- visualize_frames.py
+++ visualize_frames.py
@@ -11,24 +11,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import skimage as ski
 
-def visualize(img, mask):
-    fig, ax = plt.subplots(ncols=2, figsize=(10, 5))
+def visualize(img, f, cropped):
+    fig, ax = plt.subplots(ncols=2,figsize=(8, 5))
     ax[0].imshow(img)
-    ax[0].set_title('Original')
-    ax[0].set_axis_off()
-    ax[1].imshow(mask)
-    ax[1].set_title('Floating type')
-    ax[1].set_axis_off()
+    ax[0].set_title('Frame # '+str(f))
+    ax[1].imshow(cropped)
+    ax[1].set_title('Cropped')
     plt.show()
 
 if __name__ == "__main__":
     
     # Load data
-    positions = np.loadtxt('data/pos.csv')
-    imgdata = np.load('data/water.npy')                                      
+    positions = np.loadtxt('translation_array.csv')
+    imgdata = np.load('air-07-18-2025-hwp-2-qwp-4.npy')                                      
     n_frames = np.shape(imgdata)[2]
     intensities = np.zeros(n_frames, dtype=float)
-
-    for frame in range(1,5):
-        image = ski.util.img_as_float(imgdata[:,:,frame]/1023.00)
-        visualize(imgdata[:,:,frame]/1023.00, image)
+    print('No. of frames : ', n_frames)
+    selected_frames = [10,25,50,75,80,98]
+    for frame in selected_frames:
+        cropped_image = ski.util.img_as_float(imgdata[:,770:985,frame]/1022.00)
+        visualize(imgdata[:,:,frame]/1022.00, frame, cropped_image)
