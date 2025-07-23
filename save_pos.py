62d9bdb0f6b44507a367c943bc1550ceb006e274
Gayatri Chandran <gpc4@illinois.edu>
2025-07-23 17:25:21 -0500

    Visualize strip in frame at an angle

--- save_pos.py
+++ save_pos.py
@@ -10,14 +10,14 @@ import numpy as np
 import csv
 
 # Define start and end positions
-start_pos = 2.5  # Input start wavelength (nm) for sweep
-end_pos = 7.5    # Input end wavelength (nm)
+start_pos = 2.8  # Input start wavelength (nm) for sweep
+end_pos = 7.2    # Input end wavelength (nm)
 
 # Create the translation array
 translation = np.arange(start_pos, end_pos, 0.05)
 
 # Save the array as a CSV file
-output_filename = "translation_array_2.csv"
+output_filename = "translation_array.csv"
 
 # Save using the CSV writer
 with open(output_filename, mode='w', newline='') as file:
--- visualize_frames.py
+++ visualize_frames.py
@@ -10,14 +10,22 @@ Gayatri 09/2024
 import numpy as np
 import matplotlib.pyplot as plt
 import skimage as ski
+from skimage import draw, io
+from skimage.util import img_as_float
+from skimage.draw import rectangle
+
+from skimage.transform import AffineTransform, warp
+
+
+
 
 def visualize(img, f, cropped):
-    data_1d = cropped.ravel() # or data_2d.flatten() or np.reshape(data_2d, -1)
-    # Plot the histogram
-    plt.hist(data_1d, bins=200, alpha=0.7)
-    plt.show()
+    # data_1d = cropped.ravel() # or data_2d.flatten() or np.reshape(data_2d, -1)
+    # # Plot the histogram
+    # plt.hist(data_1d, bins=200, alpha=0.7)
+    # plt.show()
 
-    fig, ax = plt.subplots(ncols=2,figsize=(5, 5))
+    fig, ax = plt.subplots(ncols=2,figsize=(8, 8))
     ax[0].imshow(img)
     ax[0].set_title('Frame # '+str(f))
     ax[1].imshow(cropped)
@@ -29,11 +37,41 @@ if __name__ == "__main__":
     
     # Load data
     positions = np.loadtxt('translation_array.csv')
-    imgdata = np.load('air-07-18-2025-hwp-2-qwp-4.npy')                                      
+    imgdata = np.load('air-07-21-2025-hwp-2.npy')                                      
     n_frames = np.shape(imgdata)[2]
     intensities = np.zeros(n_frames, dtype=float)
     print('No. of frames : ', n_frames)
-    selected_frames = [50,75]
+    selected_frames = [5,10,20,30,40,50,60,70,80,85]
+    # selected_frames = [5]
+    #####
+    # Define center and angle
+    sample = img_as_float(np.maximum(imgdata[:,:,2], imgdata[:,:,80]))
+
+    # Step 1: Draw a line to define angle and center
+    fig, ax = plt.subplots()
+    ax.imshow(sample, cmap='gray')
+    plt.title("Click two points to define a rotated crop")
+    pts = plt.ginput(1)
+    plt.close()
+
+    # Step 2: Compute angle and center
+    print(pts)
+    (x,y) = pts[0]
+    print(x,y)
+    # print(center[0])
+    angle = 10  # degrees
+
+    # Create transform: rotation around center
+    # Rotate around center (cx, cy)
+    t1 = AffineTransform(translation=(-x,-y))               # move center to origin
+    t2 = AffineTransform(rotation=np.deg2rad(angle))       # rotate
+    t3 = AffineTransform(translation=(x,y))                 # move center back
+        
+    total_tform = t1 + t2 + t3
+
+    #####
     for frame in selected_frames:
-        cropped_image = ski.util.img_as_float(imgdata[:,770:985,frame])
-        visualize(imgdata[:,:,frame]/1022.00, frame, cropped_image)
+        # Crop axis-aligned rectangle around the center in rotated image
+        
+        cropped_image = warp(img_as_float(imgdata[:, :, frame]), total_tform.inverse)
+        visualize(imgdata[:,:,frame]/1022.00, frame, cropped_image[0:900,750:900])
